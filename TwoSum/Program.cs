b2c002318ade10399f18ca4cdbe9d608781f4ff9
galzer <galzer@outlook.com>
2022-12-29 20:49:25 -0400

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ AUTHOR: Gabriel Aquino
+ DATE: 12/29/2022
+ PURPOSE: https://leetcode.com/problems/two-sum/
+ */
+using TwoSum;
+
+Solution mySolution= new Solution();
+int[] result = mySolution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+Array.ForEach(result, Console.WriteLine);
+Console.WriteLine();
+
+result = mySolution.TwoSum(new int[] { 3, 2, 4 }, 6);
+Array.ForEach(result, Console.WriteLine);
+Console.WriteLine();
+
+result = mySolution.TwoSum(new int[] { 3, 3 }, 6);
+Array.ForEach(result, Console.WriteLine);
+Console.WriteLine();
+
+result = mySolution.TwoSum(new int[] { 2, 5, 5, 11 }, 10);
+Array.ForEach(result, Console.WriteLine);
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ AUTHOR: Gabriel Aquino
+ DATE: 12/29/2022
+ PURPOSE: https://leetcode.com/problems/two-sum/
+ */
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int indexNum1 = 0;
+            int indexNum2 = 0;
+
+            for (int i = 0; i < nums.Length -1; i++)
+            {
+                indexNum1 = i;
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    indexNum2= j;
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { indexNum1, indexNum2 };
+                }                
+            }
+            return new int[0];
+        }
+    }
+}
